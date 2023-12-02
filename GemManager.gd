081999d069569145c6e76a2f9c9d85475b8036af
steamboatsonic <steamboatsonic@gmail.com>
2023-12-02 12:29:54 -0500

    Making Gems
    - Made MetaData for gems (GemParent)
    - GemManager generates a Gem, trying to set metadata as well
    - Debug now runs in a window, and debug text is bigger

--- GemManager.gd
+++ GemManager.gd
@@ -9,11 +9,17 @@ var todaysTime: float = 0;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	var gems = [1];
+	# gems[0] = $GemParent.Duplicate();
+	#Generate the Gems
+	gems[0] = $GemParent.duplicate();
+	# gems[0].set_meta(TimingMSec, 10000.00);
+	
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	# var startTime = Get_Ticks_MSec ();
 	todaysTime = Time.get_unix_time_from_system() - startTime;
 	get_node("../../DebugText").text = str(todaysTime);
-	pass
+	
