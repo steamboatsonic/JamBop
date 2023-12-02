8d29fbfaea724b79f154417e1f256aeb512157c0
steamboatsonic <steamboatsonic@gmail.com>
2023-12-01 20:32:30 -0500

    Time Keeps on Slipping
    - Time is tracked now!
    - And the DebugText shows this!

--- GemManager.gd
+++ GemManager.gd
@@ -1,12 +1,19 @@
 extends Node
 
+# var startTime: int = 0;
+var startTime: float = Time.get_unix_time_from_system();
+# "today" is this frame
+# "yesterday" is the previous frame 
+# "tomorrow" is the expected next frame
+var todaysTime: float = 0;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var startTime: int = 0;
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	# var startTime = Get_Ticks_MSec ();
+	todaysTime = Time.get_unix_time_from_system() - startTime;
+	get_node("../../DebugText").text = str(todaysTime);
 	pass
