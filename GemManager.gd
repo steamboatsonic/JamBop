bbdc512b85d5c9eef758af35450a2597715cdbf2
steamboatsonic <steamboatsonic@gmail.com>
2023-12-03 21:26:43 -0500

    Making Gems with Metadata
    - Added Metadata to Gems
    - Instantiate a gem (but it doesn't appear yet)
    - method that writes in-game debug text

--- GemManager.gd
+++ GemManager.gd
@@ -6,20 +6,48 @@ var startTime: float = Time.get_unix_time_from_system();
 # "yesterday" is the previous frame 
 # "tomorrow" is the expected next frame
 var todaysTime: float = 0;
+var gems = [1];
+
+const gemParent = preload("res://gem_parent.tscn");
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var gems = [1];
+	makeGem();
+	
+	# var gemParent = $GemParent;
+	# var gemManager = $GemManager;
+	# var gems = [1];
 	# gems[0] = $GemParent.Duplicate();
 	#Generate the Gems
-	gems[0] = $GemParent.duplicate();
+	
+	# gems[0] = gemParent.duplicate();
+	# gems[0].position.x = 0;
+	
+	# $GemManager.add_child(gems[0]);
+	# gemManager.add_child(gems[0])
+	# add_child(gems[0]);
+	# gems[0].Set_owner(gemManager);
+	# gems[0].add_child(gemManager);
 	# gems[0].set_meta(TimingMSec, 10000.00);
+	pass
+	
+func makeGem():
+	var gem = gemParent.instantiate();
+	get_parent().add_child(gem);
+	gem.position = Vector2(100,100);
+	gem.set_meta("TimingMSec", 10.000);
+	# writeDebug(str(gem.position));
+	writeDebug(str(gem.get_meta("TimingMSec")));
 	
 	
+	# instance.position = vector2(20,20);
+	# add_child(instance);
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	# var startTime = Get_Ticks_MSec ();
 	todaysTime = Time.get_unix_time_from_system() - startTime;
-	get_node("../../DebugText").text = str(todaysTime);
+	# get_node("../../DebugText").text = str(todaysTime);
 	
+func writeDebug(textToWrite:String):
+	get_node("../../DebugText").text = textToWrite;
