7c1b8d43670ef97ce0c353df9d35f7d21b5986e4
steamboatsonic <steamboatsonic@gmail.com>
2023-12-04 19:17:19 -0500

    Gem Scripting and Metadata
    - New Gem script that every gem will use
    - Metadata for Gems and GemManager

--- GemManager.gd
+++ GemManager.gd
@@ -8,10 +8,12 @@ var startTime: float = Time.get_unix_time_from_system();
 var todaysTime: float = 0;
 var gems = [1];
 
+
 const gemParent = preload("res://gem_parent.tscn");
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	set_meta("startTime", Time.get_unix_time_from_system());
 	makeGem();
 	
 	# var gemParent = $GemParent;
@@ -33,7 +35,8 @@ func _ready():
 	
 func makeGem():
 	var gem = gemParent.instantiate();
-	get_parent().add_child(gem);
+	# get_parent().add_child(gem);
+	self.add_child(gem);
 	gem.position = Vector2(100,100);
 	gem.set_meta("TimingMSec", 10.000);
 	# writeDebug(str(gem.position));
@@ -47,6 +50,7 @@ func makeGem():
 func _process(_delta):
 	# var startTime = Get_Ticks_MSec ();
 	todaysTime = Time.get_unix_time_from_system() - startTime;
+	set_meta("todaysTime", (Time.get_unix_time_from_system() - get_meta("startTime")));
 	# get_node("../../DebugText").text = str(todaysTime);
 	
 func writeDebug(textToWrite:String):
